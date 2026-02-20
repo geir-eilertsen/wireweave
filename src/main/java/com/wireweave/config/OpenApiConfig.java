29d5c924e205b43d0310cbf57c38b46028aa54dd
Geir Eilertsen <eilertsen@corporater.com>
2026-02-20 08:45:03 +0100

    Add OpenAPI support with Springdoc; configure basic API documentation template

---
+++ src/main/java/com/wireweave/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.wireweave.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI wireWeaveOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("WireWeave API")
+                        .description("Effortless WireGuard mesh networking")
+                        .version("1.0.0"));
+    }
+}
